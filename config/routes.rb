aedf55d07174619cbcb0bd9bb39f722f3594a8b4
ltviet100 <ltviet100@gmail.com>
2019-02-14 14:55:30 +0700

    Create controllers, views for project

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,14 @@
 Rails.application.routes.draw do
+  root 'books#index'
+
+  get 'pages/about'
+
+  resources :books
+
+  resources :authors
+
+  resources :categories
+
+  resources :publishers
+
 end
